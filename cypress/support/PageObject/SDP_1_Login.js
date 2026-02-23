d21ec0a50fcd4657498fe87b925436a6d904678c
lliurm21p-lang <lliurm21p@gmail.com>
2026-02-23 14:41:30 -0500

    test 2

--- cypress/support/PageObject/SDP_1_Login.js
+++ cypress/support/PageObject/SDP_1_Login.js
@@ -7,7 +7,10 @@ class LoginSDP {
   get passwordInput() { return cy.get('[data-test="password"]'); }
   get loginButton() { return cy.get('[data-test="login-button"]'); }
 
-  login(user_name, user_password) {
+  login(
+    user_name = Cypress.env('username'), 
+    user_password = Cypress.env('password')
+  ) {
     this.usernameInput.type(user_name);
     this.passwordInput.type(user_password, { log: false });
     this.loginButton.click();
