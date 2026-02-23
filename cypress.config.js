5585a61c8cf1ef507c9d2909cdbede9787312df2
lliurm21p-lang <lliurm21p@gmail.com>
2026-02-23 13:01:25 -0500

    turn off paralelization

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
-    experimentalStudio: true,
     setupNodeEvents(on, config) {
             require('cypress-mochawesome-reporter/plugin')(on);
             return config;
